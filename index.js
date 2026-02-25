795879bec96e9347f4287bda4805dd931cb511f7
TalesNotTails <c.jetta2014@gmail.com>
2026-02-24 22:32:49 -0500

    lint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags, ActivityType } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { status } = require('minecraft-server-util');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection()
+client.commands = new Collection();
 
 // Import slash commands
-const commandPath = path.join(__dirname, 'commands')
+const commandPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -17,15 +17,16 @@ for (const file of commandFiles) {
   const command = require(filePath);
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
-  } else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  } 
+  else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
 // Listen for interactions
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-	// console.log(interaction);
+  // console.log(interaction);
   const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
