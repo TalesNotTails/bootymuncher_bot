53740c58d4c565068396dceae273ed720631fa6e
TalesNotTails <c.jetta2014@gmail.com>
2026-02-24 22:22:26 -0500

    lint

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,7 +13,8 @@ for (const file of commandFiles) {
   const command = require(filePath);
   if ('data' in command && 'execute' in command) {
     commands.push(command.data.toJSON());
-  } else {
+  } 
+  else {
     console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
   }
 }
@@ -23,15 +24,16 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID), { body: commands });
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID), { body: commands });
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} 
+  catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
 	}
 })();
