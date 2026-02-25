a65ef6df7b2953cc86c5e0f9d6f815e36fd91be8
TalesNotTails <c.jetta2014@gmail.com>
2026-02-24 22:35:21 -0500

    lint

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ for (const file of commandFiles) {
   const command = require(filePath);
   if ('data' in command && 'execute' in command) {
     client.commands.set(command.data.name, command);
-  } 
+  }
   else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
 }
 
 // Listen for interactions
@@ -30,13 +30,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
   const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
+  try {
+    await command.execute(interaction);
+  } 
+  catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
 			await interaction.followUp({
